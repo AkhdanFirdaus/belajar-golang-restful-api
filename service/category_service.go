39f470706045e1dd299747b962901736d47ccc5e
Akhdan Musyaffa Firdaus <akhdan.musyaffa.firdaus@gmail.com>
2024-01-29 00:17:25 +0700

    Category Service Contract

---
+++ service/category_service.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"context"
+
+	"github.com/AkhdanFirdaus/belajar-golang-restful-api/model/web"
+)
+
+type CategoryService interface {
+	Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
+	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
+	Delete(ctx context.Context)
+	FindById(ctx context.Context) web.CategoryResponse
+	FindAll(ctx context.Context) []web.CategoryResponse
+}
